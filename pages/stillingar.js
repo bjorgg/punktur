26fcdaa77726c7b148d84b4788636fc1e5f3eaa8
IrisElva <iriselva@gmail.com>
2021-03-23 20:18:12 +0000

    global styles, form and modal

--- pages/stillingar.js
+++ pages/stillingar.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Form.module.css";
 import { useCurrentUser } from "../hooks/user";
 import React, { useState, useEffect, useRef } from "react";
 import Modal from "../components/modal";
@@ -93,7 +93,7 @@ export default function Settings() {
                     <form onSubmit={handleSubmit} ref={formRef}>
                         {msg.message ? <p style={{ color: msg.isError ? "red" : "#0070f3", textAlign: "center" }}>{msg.message}</p> : null}
                         <div>
-                            {!user.avatar ? 
+                            {/* {!user.avatar ? 
                                 <Image
                                     src={defaultAvatar}
                                     alt="Avatar"
@@ -106,28 +106,27 @@ export default function Settings() {
                                     height="100" 
                                     alt={user.username} 
                                 />
-                            }
+                            } */}
                             <label htmlFor="avatar">
-                                Veldu prófílmynd
-                                <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
+                                <input className={styles.customFileInput} type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
                             </label>
                         </div>
 
                         <div>
                             <label htmlFor="username">
-                                Nafn/Höfundarnafn
-                                <input required id="username" name="username" type="text" />
+                                <h5>Nafn/Höfundarnafn</h5>
+                                <input className={styles.formInput} required id="username" name="username" type="text" />
                             </label>
                         </div>
                         <div>
                             <label htmlFor="email">
-                                Netfang
-                                <input id="email" name="email" type="email" />
+                                <h5>Netfang</h5>
+                                <input className={styles.formInput} id="email" name="email" type="email" />
                             </label>
                         </div>
                         <div>
                             <label htmlFor="bio">
-                                Um mig
+                                <h5>Um mig</h5>
                                 <textarea id="bio" name="bio" maxLength="50" />
                             </label>
                         </div>
@@ -136,9 +135,10 @@ export default function Settings() {
                         </button>
                     </form>
 
-                    <button onClick={() => setModalOpen(true)}>Eyða aðgangi</button>
-                    <Modal show={isOpen} title="Ertu viss um að þú viljir eyða aðgangi þínum?" onSubmit={handleDeleteUser} onClose={() => setModalOpen(false)} submitText="Já" cancelText="Nei, hætta við">
-                        <p>Her er texti sem þú vilt setja i dialoginn</p>
+                    <button 
+                        onClick={() => setModalOpen(true)}>Eyða aðgangi</button>
+                    <Modal show={isOpen} title="Eyða aðgangi?" onSubmit={handleDeleteUser} onClose={() => setModalOpen(false)} submitText="Já" cancelText="Nei">
+                        <p>Allar upplýsingar verða eyddar út af punkti</p>
                     </Modal>
                 </div>
             )}
