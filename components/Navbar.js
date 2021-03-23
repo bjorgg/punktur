641c927b72b8324c6cef20480400a5022a8a02fd
Kollaaj <kolbrunaj@gmail.com>
2021-03-23 15:04:04 +0000

    Styled header and navbar - mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { useCurrentUser } from "../hooks/user";
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
     const [user, { mutate }] = useCurrentUser();
@@ -13,42 +15,81 @@ export default function Navbar() {
         mutate(null);
     };
     return (
-        <nav>
-            <div>
+        <nav className={styles.nav}>
+            <div className={styles.logo}>
                 {!user ? '' : (
-                    <Link href="/ny-saga">
-                        <a>Ný saga</a>
+                    <Link href="/">
+                        <Image  
+                            src="/Icons/NavbarLogo.svg"
+                            alt="heim"
+                            width={32}
+                            height={32}/>
                     </Link>
                 )}
             </div>
-            <Link href="/um-punkt">
-                <a>Um punkt</a>
-            </Link>
-            <div>
+            <div className={styles.write}>
+                {!user ? '' : (
+                    <Link href="/ny-saga"  >
+                        <Image  
+                            src="/Icons/write.svg"
+                            alt="Ný saga"
+                            width={32}
+                            height={32}/>
+                    </Link>
+                )}
+            </div>
+            <div className={styles.user}>
                 {!user ? '' : (
                     <Link href="/min-sida">
-                        <a>Mín síða</a>
+                        <Image  
+                            src="/Icons/User.svg"
+                            alt="mín síða"
+                            width={32}
+                            height={32}/>
                     </Link>
                 )}
             </div>
-            <div>
+            {/* <Link href="/um-punkt">
+                {/* <a>Um punkt</a> */}
+                {/* <Image  
+                            src="/Icons/User.svg"
+                            alt="um punkt"
+                            width={32}
+                            height={32}/> */}
+            {/* </Link>  */}
+            {/* <div>
                 {!user ? (
                     <Link href="/skra-inn">
-                        <a>Skrá inn</a>
+                        {/* <a>Skrá inn</a> */}
+                        {/* <Image  
+                            src="/Icons/SignIn.svg"
+                            alt="skrá inn"
+                            width={32}
+                            height={32}/>
                     </Link>
-                ) : (
-                    <a tabIndex={0} role="button" onClick={handleLogout}>
-                        Skrá út
-                    </a>
-                )}
-            </div>
-            <div>
+                ) : ( */}
+                    {/* <a tabIndex={0} role="button" onClick={handleLogout}>
+                        {/* Skrá út */}
+                        {/* <Image  
+                            src="/Icons/SignOut.svg"
+                            alt="skrá út"
+                            width={32}
+                            height={32}/>
+                    </a> */}
+            {/*                 
+            </div> */}
+            {/* <div>
                 {!user ? '' : (
                     <Link href="/stillingar">
-                        <a>Stillingar</a>
+                        {/* <a>Stillingar</a> */}
+                        {/* <Image  
+                            src="/Icons/stillingar.svg"
+                            alt="Stillingar"
+                            width={32}
+                            height={32}/>
                     </Link>
-                )}
-            </div>
+                )} */}
+            {/* </div> */}
         </nav>
     );
 }
