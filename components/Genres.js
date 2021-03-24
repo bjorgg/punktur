022ae4d8eb1006834f40f373fda68d10dde560bb
Kollaaj <kolbrunaj@gmail.com>
2021-03-24 09:19:43 +0000

    genres tags styled

--- components/Genres.js
+++ components/Genres.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from "../styles/tag.module.css";
+import Image from 'next/image'
 
 
 export default function Genres({ onClick }) {
@@ -20,13 +22,20 @@ export default function Genres({ onClick }) {
   ] 
 
   return (
-    <div>
+    <div class="tags" className={styles.tags}>
       {Genres &&
         Genres.map((genre, i) => (
-            <div id='storyGenres' key={ genre }>
-              <input id={`genre${i}`} type="checkbox" name="genre" value={ genre } onClick={onClick}/>
-              <label htmlFor={`genre${i}`}>{ genre }</label>
+          <div>
+            <div className={styles.tag} id='storyGenres' key={ genre }>
+              <input id={`genre${i}`} className={styles.inputHide} type="checkbox" name="genre" value={ genre } onClick={onClick}/>
+              <label className={styles.label} htmlFor={`genre${i}`}>{ genre }</label>
+              <Image  
+                src="/img/triangle.svg"
+                alt=""
+                width={19}
+                height={30}/>
             </div>
+          </div>
         ))}
     </div>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import MyLink from "./MyLink";
 import { useCurrentUser } from "../hooks/user";
 import Image from 'next/image'
 import styles from '../styles/Navbar.module.css'
@@ -18,55 +18,55 @@ export default function Navbar() {
         <nav className={styles.nav}>
             <div className={styles.logo}>
                 {!user ? '' : (
-                    <Link href="/">
+                    <MyLink href="/">
                         <Image  
                             src="/Icons/NavbarLogo.svg"
                             alt="heim"
                             width={32}
                             height={32}/>
-                    </Link>
+                    </MyLink>
                 )}
             </div>
             <div className={styles.write}>
                 {!user ? '' : (
-                    <Link href="/ny-saga"  >
+                    <MyLink href="/ny-saga" className={styles.abc} >
                         <Image  
                             src="/Icons/write.svg"
                             alt="Ný saga"
                             width={32}
                             height={32}/>
-                    </Link>
+                    </MyLink>
                 )}
             </div>
             <div className={styles.user}>
                 {!user ? '' : (
-                    <Link href="/min-sida">
+                    <MyLink href="/min-sida">
                         <Image  
                             src="/Icons/User.svg"
                             alt="mín síða"
                             width={32}
                             height={32}/>
-                    </Link>
+                    </MyLink>
                 )}
             </div>
-            {/* <Link href="/um-punkt">
+            {/* <MyLink href="/um-punkt">
                 {/* <a>Um punkt</a> */}
                 {/* <Image  
                             src="/Icons/User.svg"
                             alt="um punkt"
                             width={32}
                             height={32}/> */}
-            {/* </Link>  */}
+            {/* </MyLink>  */}
             {/* <div>
                 {!user ? (
-                    <Link href="/skra-inn">
+                    <MyLink href="/skra-inn">
                         {/* <a>Skrá inn</a> */}
                         {/* <Image  
                             src="/Icons/SignIn.svg"
                             alt="skrá inn"
                             width={32}
                             height={32}/>
-                    </Link>
+                    </MyLink>
                 ) : ( */}
                     {/* <a tabIndex={0} role="button" onClick={handleLogout}>
                         {/* Skrá út */}
@@ -80,14 +80,14 @@ export default function Navbar() {
             </div> */}
             {/* <div>
                 {!user ? '' : (
-                    <Link href="/stillingar">
+                    <MyLink href="/stillingar">
                         {/* <a>Stillingar</a> */}
                         {/* <Image  
                             src="/Icons/stillingar.svg"
                             alt="Stillingar"
                             width={32}
                             height={32}/>
-                    </Link>
+                    </MyLink>
                 )} */}
             {/* </div> */}
         </nav>
